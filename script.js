9b1fde80791555ffde96bc4cd4393d7b2b4df635
hardevkoder <haroldvaldes@yahoo.com>
2023-06-30 21:26:28 -0500

    Eliminacion de carga retardada de script

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-
-function downloadJSAtOnload() {
-    // Escribir aquí el codigo JS de la APP
-    var contador=0
+    let contador=0
     document.getElementById('disminuir').addEventListener('click',()=>{
         --contador;
         document.getElementById('cuenta').textContent=contador;
@@ -11,14 +8,9 @@ function downloadJSAtOnload() {
        ++contador;
        document.getElementById('cuenta').textContent=contador;
     })
+
     document.getElementById('reiniciar').addEventListener('click',()=>{
         contador = 0;
         document.getElementById('cuenta').textContent=contador;
     })
-}
 
-if (window.addEventListener)
-    window.addEventListener("load", downloadJSAtOnload, false);
-else if (window.attachEvent)
-    window.attachEvent("onload", downloadJSAtOnload);
-else window.onload = downloadJSAtOnload;
